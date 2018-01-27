0ce44d1a515847d6298ee2f8a65c24e5bfeb3a16
Thiago Colen <thiago.souzacolen@gmail.com>
2018-01-27 20:27:29 -0200

    add-autocomplete-address

--- src/components/MyGMapComponent/MyGMapComponent.js
+++ src/components/MyGMapComponent/MyGMapComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 
-
 class MyGMapComponent extends Component {  
 
   constructor(props) {
@@ -11,46 +10,12 @@ class MyGMapComponent extends Component {
     }
   }
 
-  initMap() {
-    console.log('initMap')
-    window.map = new window.google.maps.Map(document.getElementById('MyGMapComponent'), {
-      // center: {lat: -23.001, lng: -47.001},
-      zoom: 18
-    });
-
-    const infoWindow = new window.google.maps.InfoWindow({map: window.map});
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Você está aqui seu bosta!');
-        window.map.setCenter(pos);
-      }, function() {
-        this.handleLocationError(true, infoWindow, window.map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      this.handleLocationError(false, infoWindow, window.map.getCenter());
-    }
-
-  }
-
-
-  handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-  }
+  //--------------------------------
+  //--------------------------------
+  //--------------------------------
 
   loadJS(src) {
-    console.log('loadJS')
+    console.log('loadJS')    
     var ref = window.document.getElementsByTagName("script")[0];
     var script = window.document.createElement("script");
     script.src = src;
@@ -58,6 +23,97 @@ class MyGMapComponent extends Component {
     script.defer = true;
     ref.parentNode.insertBefore(script, ref);
   }
+
+  initMap() {
+    console.log('initMap')
+    this.map = new window.google.maps.Map(document.getElementById('MyGMapComponent'), {
+      // center: {lat: -20.001, lng: -47.001},
+      zoom: 14
+    });
+    
+    this.initGeolocation()
+    this.initAutocomplete()
+  }
+
+  initGeolocation() {
+    console.log('initInfoWindow')
+
+    const successCallback = (position) => { 
+      this.currentPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      infoWindow.setPosition(this.currentPosition);
+      infoWindow.setContent('Você está aqui seu bosta!');        
+      this.map.setCenter(this.currentPosition);   
+    }
+
+    const errorCallback = () => {
+      handleLocationError(true, infoWindow, this.map.getCenter());      
+    }
+
+    const handleLocationError = (browserHasGeolocation, infoWindow, pos) => {
+      console.log('handleLocationError')
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    }
+  
+    const infoWindow = new window.google.maps.InfoWindow({map: this.map});
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      handleLocationError(false, infoWindow, this.map.getCenter());
+    }
+  }
+
+  initAutocomplete() {
+    console.log('initAutocomplete');
+    // Create the autocomplete object, restricting the search to geographical location types.
+    this.autocomplete = new window.google.maps.places.Autocomplete(
+      (document.getElementById('autocomplete')), 
+      {types: ['geocode']}
+    );
+
+    // When the user selects an address from the dropdown, populate the address fields in the form.
+    this.autocomplete.addListener('place_changed', this.fillInAddress.bind(this));
+  }
+      
+  // renomear, isso tá estranho
+  fillInAddress() {
+    var place = this.autocomplete.getPlace();
+
+    this.setState({
+      addressPosition: {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng()
+      }
+    });
+  }
+
+  geolocate() {
+    const successCallback = (position) => {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new window.google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      this.autocomplete.setBounds(circle.getBounds());
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback);
+    }
+  }
+
+  //--------------------------------
+  //--------------------------------
+  //--------------------------------
 
   componentWillMount () {
     console.log('componentWillMount')
@@ -75,30 +131,48 @@ class MyGMapComponent extends Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount')    
-    window.initMap = this.initMap;
-    this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyCv0MDuPwY5ruP5YWb9fvdaT0Ha63fhSN8&callback=initMap')
+    console.log('componentDidMount')   
+    window.initMap = this.initMap.bind(this);
+    this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyCv0MDuPwY5ruP5YWb9fvdaT0Ha63fhSN8&libraries=places&callback=initMap')
   }
 
   componentWillUpdate(nextProps, nextState) {
-    console.log('componentWillUpdate')
-    nextState.places.map((item) => {
-      new window.google.maps.Marker({
-        position: {lat: item.lat, lng: item.lng},
-        map: window.map
-      });
-    })
+    console.log('componentWillUpdate', nextState.addressPosition)
+
+    // todo: pegar os places do firebase    
+    // nextState.places.map((item) => {
+    //   new window.google.maps.Marker({
+    //     position: {lat: item.lat, lng: item.lng},
+    //     map: window.map
+    //   });
+    // })
+
+    if (nextState.addressPosition) {
+      this.addressPosition = {
+        lat: nextState.addressPosition.lat,
+        lng: nextState.addressPosition.lng
+      }
+  
+      const infoWindow = new window.google.maps.InfoWindow({map: this.map});    
+      infoWindow.setPosition(this.addressPosition);
+      infoWindow.setContent('Agora você está aqui seu cuzão!');        
+      this.map.setCenter(this.addressPosition);
+    }
+
+
   }
-
-
-
-
 
   render() {
     return(
       <div>
-        <h1>MyGMapComponent</h1>
-        <div id="MyGMapComponent" style={{ border:'solid 10px black', height:'600px' }}></div>
+        <div id="MyGMapComponent" 
+          style={{ border:'solid 20px black', height:'600px' }}></div>
+        <br/>
+        <input id="autocomplete" 
+          placeholder="Enter your address"
+          onFocus={this.geolocate()} 
+          type="text"
+          style={{ height:'30px', width:'400px' }}></input>
       </div>
     )
   }
